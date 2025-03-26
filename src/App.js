d98eb145a481e2d7be75cc7eabede02f1c0ad097
Sunita <sunitamousaviii@gmail.com>
2025-03-26 20:32:00 +0330

    footer

--- src/App.js
+++ src/App.js
@@ -20,8 +20,8 @@ export default function App() {
           target="_blank"
           rel="noopener noreferrer">
           GitHub
-        </a>
-        and hosted on
+        </a>{" "}
+        and hosted on{" "}
         <a
           href="https://reactweatherapp-sunitamousavi.netlify.app"
           target="_blank"
