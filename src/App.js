e7d7bdfb262a19d76da57579bddcff430d6de27f
Sunita <sunitamousaviii@gmail.com>
2025-03-26 20:30:12 +0330

    added the footer

--- src/App.js
+++ src/App.js
@@ -7,7 +7,27 @@ export default function App() {
     <div className="App">
       <Weather />
       <footer className="text-center">
-        Coded by Sunita Mousavi and hosted on Netlify
+        Coded by{" "}
+        <a
+          href="https://github.com/SunitaMousavi"
+          target="_blank"
+          rel="noopener noreferrer">
+          Sunita Mousavi
+        </a>
+        , open-sourced on{" "}
+        <a
+          href="https://github.com/SunitaMousavi/react-weather"
+          target="_blank"
+          rel="noopener noreferrer">
+          GitHub
+        </a>
+        and hosted on
+        <a
+          href="https://reactweatherapp-sunitamousavi.netlify.app"
+          target="_blank"
+          rel="noopener noreferrer">
+          Netlify
+        </a>
       </footer>
     </div>
   );
